a0cf496ac309a5fafae0ce6d945de4e91cccef58
Krischo99 <kristiyan18kiko@gmail.com>
2024-04-17 19:52:03 +0300

    update functions
    complete app functionality

--- First Project/Money_Dashboard/Money-Tracker/build/gui.py
+++ First Project/Money_Dashboard/Money-Tracker/build/gui.py
@@ -17,27 +17,53 @@ ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\Christian\Desktop\Python projects\Fi
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
-
 window = Tk()
 
-express = 560
+# Format and display data
+def add_Sum_And_Calculate_Display(some_var, some_id_text):
+    new_income_rounded = round(some_var, 2)
+    some_var = new_income_rounded
+    
+    new_text_display = "${:.2f}".format(new_income_rounded)
+    canvas.itemconfig(tagOrId=some_id_text, text=new_text_display)
+    return some_var
 
-def add_Sum_To_Expense():
-    global express
+def check_For_Name():
+    # Make our variables global cuz we need to access them
+    global income, expense, balance
     try:
+        name = entry_1.get().lower()
+        # Take input and convert it to double sum
         new_income = float(entry_2.get())
-        new_formated = express + new_income
-        new_income_rounded = round(new_formated, 2) 
-        express = new_income_rounded
-        
-        new_text_display = "${:.2f}".format(new_income_rounded)
-        canvas.itemconfig(tagOrId=expenses_text, text=new_text_display)
+                
+        if name == "add" or name == "income":
+            # Sum our new income with our current income value
+            income += new_income
+            # Equalize sums
+            income = add_Sum_And_Calculate_Display(income, income_text)
+            # Calculate our balance
+            balance = income - expense
+            balance = add_Sum_And_Calculate_Display(balance, balance_text)
+        elif name == "subtract" or name == "expense":
+            expense += new_income
+            expense = add_Sum_And_Calculate_Display(expense, expense_text)
+            balance = income - expense
+            balance = add_Sum_And_Calculate_Display(balance, balance_text)
+        else:
+            messagebox.showinfo("ERROR", "Check if your inputs are correct!")
     except:
-        pass
+        messagebox.showinfo("ERROR", "Check if your inputs are correct!")
+
+# Get info from "canvas.create_text" and save it
+def convert_Sum_To_Float(some_text):
+    text_property = canvas.itemconfig(some_text, "text")
+    # Taka canvas configuration from "text" field
+    float_text = text_property[4].lstrip("$")
+    # We cut "$" symbol because we cant convert it to float num
+    return float_text
 
 window.geometry("720x605")
 window.configure(bg = "#FFFFFF")
-
 
 canvas = Canvas(
     window,
@@ -118,32 +144,40 @@ canvas.create_text(
     font=("Inter ExtraBold", 15 * -1)
 )
 
-canvas.create_text(
+
+
+# Save as variable that we want to change
+income_text = canvas.create_text(
     69.0,
     139.0,
     anchor="nw",
-    text="$5.45",
+    text="$0.00",
     fill="#000000",
     font=("Inter ExtraBold", 24 * -1)
 )
+income = float(convert_Sum_To_Float(income_text))
 
-canvas.create_text(
+balance_text = canvas.create_text(
     69.0,
     231.0,
     anchor="nw",
-    text="$50.27",
+    text="$0.00",
     fill="#000000",
     font=("Inter ExtraBold", 24 * -1)
 )
+balance = float(convert_Sum_To_Float(balance_text))
 
-expenses_text = canvas.create_text(
+expense_text = canvas.create_text(
     410.0,
     141.0,
     anchor="nw",
-    text="$560.45",
+    text="$0.00",
     fill="#000000",
     font=("Inter ExtraBold", 24 * -1)
 )
+expense = float(convert_Sum_To_Float(expense_text))
+
+
 
 canvas.create_text(
     54.0,
@@ -226,7 +260,7 @@ button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: add_Sum_To_Expense(),
+    command=lambda: check_For_Name(),
     relief="flat"
 )
 button_1.place(
